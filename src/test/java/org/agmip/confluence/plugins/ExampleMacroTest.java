50cd66a5ba243f3a107dd223620954452b50d3b0
Christopher Villalobos <christopher.villalobos@gmail.com>
2012-06-08 17:21:09 -0400

    Initial Commit

---
+++ src/test/java/org/agmip/confluence/plugins/ExampleMacroTest.java
@@ -0,0 +1,15 @@
+package org.agmip.confluence.plugins;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link org.agmip.confluence.plugins.ExampleMacro}
+ */
+public class ExampleMacroTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
